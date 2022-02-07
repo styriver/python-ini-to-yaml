ff5e5370c24184c1533258c03dca2ee03ff01606
styriver <styriver@gmail.com>
2022-02-07 09:09:40 -0600

    added for splunk conf

--- ini_to_yml.py
+++ ini_to_yml.py
@@ -1,31 +1,54 @@
 #!/usr/bin/env python
 
-import ConfigParser
+import configparser
 import yaml
 import argparse
 
-def process_ini(inifile,ymlfile):
-    config = ConfigParser.ConfigParser()
+
+def process_ini(inifile, ymlfile):
+    config = configparser.ConfigParser(allow_no_value=True)
     config.read(inifile)
-   
-    datamap = {} 
+
+    datamap = {}
     for section in config.sections():
         datamap[section] = {}
-        for name,value in config.items(section):
-            datamap[section].update({name:value})
+        if len(config.items(section)) == 0:
+            datamap[section].update({"placeholder": "Puppet does not support empty sections"})
+        else:
+            for name, value in config.items(section):
+                datamap[section].update({name: value})
 
-    with open(ymlfile,"w") as ymlfile:
-        yaml.dump(datamap,ymlfile,default_flow_style=False)     
+    with open(ymlfile, "w") as tmpYmlfile:
+        yaml.dump(datamap, tmpYmlfile, default_flow_style=False)
+
+    with open(ymlfile) as file:
+        lines = file.readlines()
+
+    count = 0
+    for line in lines:
+        string_length = len(line)+4    # will be adding 4 extra spaces
+        lines[count]=line.rjust(string_length)
+        count += 1
+
+    lines.insert(0, "dir:\n")
+    lines.insert(1, "  outputs:\n")
+
+    modified_yaml = open(ymlfile+"mod", "w")
+    for element in lines:
+        modified_yaml.write(element)
+
+    modified_yaml.close()
 
 def main():
     parser = argparse.ArgumentParser(description="Convert a basic ini file to yml")
-    parser.add_argument('--in',action="store",dest="ini",required=True,help="Input ini file")
-    parser.add_argument('--out',action="store",dest="yml",required=True,help="Output yml file")
+    parser.add_argument('--in', action="store", dest="ini", required=True, help="Input ini file")
+    parser.add_argument('--out', action="store", dest="yml", required=True, help="Output yml file")
     args = vars(parser.parse_args())
     inifile = args['ini']
     ymlfile = args['yml']
 
-    process_ini(inifile,ymlfile) 
+    process_ini(inifile, ymlfile)
+
 
 if __name__ == '__main__':
     main()
