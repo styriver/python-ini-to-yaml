ef792b3fbc8f698bd91e9c513eb13f836507a680
styriver <styriver@gmail.com>
2022-02-07 18:46:55 -0600

    changed line add

--- ini_to_yml.py
+++ ini_to_yml.py
@@ -3,11 +3,16 @@
 import configparser
 import yaml
 import argparse
+from pathlib import Path
+import os
 
-
-def process_ini(inifile, ymlfile):
+def process_ini(inifile):
+    file_name = Path(inifile).stem
     config = configparser.ConfigParser(allow_no_value=True)
     config.read(inifile)
+    file_name = Path(inifile).stem
+    file_path = str(Path(inifile).parent)
+    yamlfile = os.path.join(file_path, file_name + ".yaml")
 
     datamap = {}
     for section in config.sections():
@@ -18,38 +23,51 @@ def process_ini(inifile, ymlfile):
             for name, value in config.items(section):
                 datamap[section].update({name: value})
 
-    with open(ymlfile, "w") as tmpYmlfile:
-        yaml.dump(datamap, tmpYmlfile, default_flow_style=False,sort_keys=False)
+    # dump yaml
+    with open(yamlfile, "w") as newYaml:
+        yaml.dump(datamap, newYaml, default_flow_style=False,sort_keys=False)
 
-    with open(ymlfile) as file:
+    # open to add conf file name and indent properly
+    with open(yamlfile, "r") as file:
         lines = file.readlines()
 
     # loop through file and indent each line properly
     count = 0
     for line in lines:
         string_length = len(line)+6    # will be adding 6 extra spaces for proper yaml indentation
-        lines[count]=line.rjust(string_length)
+        lines[count] = line.rjust(string_length)
         count += 1
 
     # add conf file name
-    lines.insert(0, "    @todo(conf file):\n")
+    lines.insert(0, "    " + file_name+": # " + file_name + ".conf\n")
 
-    modified_yaml = open(ymlfile+"mod", "w")
-#    modified_yaml.write("#\n# conf files under ./system/local\n#\n  @todo(dirname):\n")
+    indented_yaml = open(yamlfile, "w")
     for element in lines:
-        modified_yaml.write(element)
+        indented_yaml.write(element)
 
-    modified_yaml.close()
+    indented_yaml.close()
+
 
 def main():
     parser = argparse.ArgumentParser(description="Convert a basic ini file to yml")
-    parser.add_argument('--in', action="store", dest="ini", required=True, help="Input ini file")
-    parser.add_argument('--out', action="store", dest="yml", required=True, help="Output yml file")
+    parser.add_argument('--in', action="store", dest="workdir", required=True, help="Input ini file")
+    parser.add_argument('--out', action="store", dest="outdir", required=True, help="Input ini file")
     args = vars(parser.parse_args())
-    inifile = args['ini']
-    ymlfile = args['yml']
+    working_dir = args['workdir']
+    out_dir = args['outdir']
+    files = Path(working_dir).glob('*.conf')
+    inifile = ''
+    for inifile in files:
+      process_ini(inifile)
 
-    process_ini(inifile, ymlfile)
+    # merge all *.conf conversions to master
+    files = Path(working_dir).glob('*.yaml')
+    with open(out_dir, 'a+') as outfile:
+        file_path = str(Path(inifile).parent)
+        outfile.write("#\n# conf files under ." + os.sep + file_path + "\n#\n  " + file_path + ":\n")
+        for file_name in files:
+            with open(file_name, 'r') as readfile:
+                outfile.write(readfile.read() + "\n")
 
 
 if __name__ == '__main__':
--- tests/test.sh
+++ tests/test.sh
@@ -3,5 +3,6 @@
 echo "installing pyyaml"
 pip install pyyaml --upgrade
 echo "running test"
-python ini_to_yml.py --in tests/example.ini --out tests/example.yml
+python ini_to_yml.py --in ./tests/PROD-AUTH --out C:\Users\HpLaptop-Scott\test.yaml
+python ini_to_yml.py --in ./tests/PROD-CLOUD --out C:\Users\HpLaptop-Scott\test.yaml
 
